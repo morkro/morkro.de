c18cc329351185663972fb04c22a027d60cb954e
Moritz Kröger <write@morkro.de>
2021-04-29 11:05:13 +0200

    Move Jekyll source code into dedicated folder

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,14 @@ const minify = require('rollup-plugin-babel-minify')
 const commonjs = require('rollup-plugin-commonjs')
 
 module.exports = {
-	input: 'scripts/main.js',
+	input: 'src/scripts/main.js',
 	output: {
-		file: 'assets/main.min.js',
-		format: 'iife'
+		file: 'src/assets/main.min.js',
+		format: 'iife',
 	},
 	plugins: [
 		resolve({ browser: true }),
 		commonjs({ include: 'node_modules/**' }),
-		minify({ comments: false })
-	]
+		minify({ comments: false }),
+	],
 }
