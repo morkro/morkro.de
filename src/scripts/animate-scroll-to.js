80e38149ffe6ad4d070cc01c64393544cea0181d
Moritz Kröger <write@morkro.de>
2021-08-31 20:48:21 +0200

    release 1.5.10: fixes scrolling

--- src/scripts/animate-scroll-to.js
+++ src/scripts/animate-scroll-to.js
@@ -1,8 +1,14 @@
-import { requestAnimFrame, easeInOutQuint, windowPosition } from './helper'
+import { $, requestAnimFrame, easeInOutQuint, windowPosition } from './helper'
 
 const animationSpeed = 333
 const animationOffset = -10
 const animationIncrement = 20
+
+function resolveNode(node) {
+	let keyword = node.getAttribute('data-scrollto')
+	if (keyword === '#') keyword = 'body'
+	return [node, $(keyword)]
+}
 
 /**
  * @param {Number} amount
@@ -17,13 +23,16 @@ function moveDocument(amount) {
  * @param {Function} callback
  */
 export default function animateScrollTo($el, callback = () => {}) {
+	const selector = $el.getAttribute('data-scrollto')
+	const $destination = selector === '#' ? $('body') : $(selector)
+
 	$el.addEventListener('click', (event) => {
 		event.preventDefault()
 		event.target.blur()
 
 		let currentTime = 0
 		const start = windowPosition()
-		const change = document.body.getBoundingClientRect().top + animationOffset
+		const change = $destination.getBoundingClientRect().top + animationOffset
 		const animateScroll = () => {
 			currentTime += animationIncrement
 			const easing = easeInOutQuint(currentTime, start, change, animationSpeed)
