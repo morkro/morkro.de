7816556d233004bfd46e8d4b232b4e604a4a88f0
Moritz Kröger <write@morkro.de>
2023-06-05 20:10:37 +0200

    fixes sass handling, removes unused jekyll files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,38 @@
+const path = require('node:path')
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const sass = require('sass')
 
 module.exports = function (config) {
 	config.setLiquidOptions({
 		dynamicPartials: false,
 	})
 
+	config.addTemplateFormats('scss')
+	config.addExtension('scss', {
+		outputFileExtension: 'css',
+		compile: function (inputContent, inputPath) {
+			const parsed = path.parse(inputPath)
+			const result = sass.compileString(inputContent, {
+				loadPaths: [parsed.dir || '.', this.config.dir.includes],
+			})
+			return () => result.css
+		},
+	})
+
 	config.addPlugin(syntaxHighlight)
 
-	config.addWatchTarget('src/css/')
+	// config.addWatchTarget('src/css/')
 	config.addWatchTarget('src/scripts/')
 
 	config.addPassthroughCopy('src/assets')
+	// config.addPassthroughCopy('src/css')
 	config.addPassthroughCopy('src/humans.txt')
+
+	config.addShortcode('currentYear', () => new Date().getFullYear())
+
+	config.addCollection('posts', function (collection) {
+		return collection.getFilteredByGlob('src/_posts/*.md')
+	})
 
 	return {
 		dir: {
---
+++ src/_data/meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+	title:
+		'Moritz Kröger 🙋 — Software Engineering Director and Open Source contributor',
+	email: 'write@morkro.de',
+	description:
+		'I am Moritz Kröger, Software Engineering Director and Open Source contributor who lives in Berlin. Although Germans typically have a thing for extra long compound words, I am a friend of short functions.',
+	baseurl: '',
+	url: 'https://morkro.de',
+}
