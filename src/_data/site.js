203634b73292dc8cef59e24b91d15b5bfe0f6212
Moritz Kröger <write@morkro.de>
2023-06-13 10:52:42 +0200

    update description and hide instagram

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ module.exports = {
 	email: 'write@morkro.de',
 	title: 'Hey there!',
 	description:
-		'I am Moritz Kröger, Software Engineering Director and Open Source contributor who lives in Berlin.',
+		'I am Moritz Kröger, Director of Software & Cloud Engineering and Open Source contributor who lives in Berlin.',
 	url: 'https://morkro.de',
 	environment: process.env.NODE_ENV,
 	timestamp: Date.now(),
