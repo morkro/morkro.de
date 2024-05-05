2593e132b071578b33a27d580d43a01d5fbf8e60
Moritz Kröger <write@morkro.de>
2024-05-05 14:07:07 +0200

    removes deprecated rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,6 @@ module.exports = {
 		sourceType: 'module',
 	},
 	rules: {
-		indent: [
-			2,
-			'tab',
-			{
-				SwitchCase: 1,
-			},
-		],
-		quotes: [2, 'single'],
-		semi: [2, 'never'],
-		'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
 		'prefer-const': 2,
 	},
 }
