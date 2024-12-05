2dd046296eee46ce297953082ae0f26dd7ad1e9c
Moritz Kröger <write@morkro.de>
2024-12-05 13:55:34 +0100

    added a tooltip to the footer explaining the versioning

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # <img src="src/assets/img/moritz-small.png" align="center" width="30" height="30"> Moritz Kröger Website [![Netlify Status](https://api.netlify.com/api/v1/badges/9cda398d-be03-491b-a9ac-afdfd2245bc1/deploy-status)](https://app.netlify.com/sites/moritz/deploys)
 
 Source code of my personal website [moritz.berlin](https://moritz.berlin). Build with [11ty](https://www.11ty.dev/).
+
+### Versioning
+
+I use custom versioning for my website, because it will just be relevant to me anyway and I wanted a system I can easily translate to the changes I usually make. I've used SemVer for a while but always struggled with it (_Is rewriting a full content section a major or minor change?_).
+
+The current format goes like: `Year.Month.Commits.Type`, with the following possible types `M1 (Major)`, `M2 (Minor)`, `P0 (Patch)`.
--- bin/pre-push.js
+++ bin/pre-push.js
@@ -1,58 +1,61 @@
 #!/usr/bin/env node
 
 import { execSync } from 'node:child_process'
-import fs from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
+import readline from 'node:readline'
+import tty from 'node:tty'
 
-const packageJsonPath = './package.json'
+const pkgPath = './package.json'
+
+// Handle non-interactive environments by assigning /dev/tty to process.stdin
+if (!process.stdin.isTTY) {
+	const { O_RDONLY, O_NOCTTY } = fs.constants
+	let fd
+	try {
+		fd = fs.openSync('/dev/tty', O_RDONLY | O_NOCTTY)
+	} catch (error) {
+		console.error('Please push your code in a terminal.')
+		process.exit(1)
+	}
+
+	const stdin = new tty.ReadStream(fd)
+
+	Object.defineProperty(process, 'stdin', {
+		configurable: true,
+		enumerable: true,
+		get: () => stdin,
+	})
+}
+
+function prompt(question) {
+	return new Promise((resolve) => {
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		})
+
+		rl.question(question, (answer) => {
+			rl.close()
+			resolve(answer.trim())
+		})
+	})
+}
 
 async function getCurrentVersion() {
-	const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'))
+	const packageJson = JSON.parse(await readFile(pkgPath, 'utf8'))
 	return packageJson.version
 }
 
 async function updateVersion(newVersion) {
-	const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'))
+	const packageJson = JSON.parse(await readFile(pkgPath, 'utf8'))
 	packageJson.version = newVersion
-	await fs.writeFile(
-		packageJsonPath,
-		JSON.stringify(packageJson, null, 2),
-		'utf8',
-	)
+	await writeFile(pkgPath, JSON.stringify(packageJson, null, 2), 'utf8')
 }
 
 function getTotalCommitsForYear() {
 	const currentYear = new Date().getFullYear()
 	const gitCommand = `git rev-list --count --since="${currentYear}-01-01" HEAD`
-	try {
-		return Number.parseInt(execSync(gitCommand).toString().trim(), 10) || 0
-	} catch (err) {
-		console.error('Error calculating commits:', err.message)
-		process.exit(1)
-	}
-}
-
-async function promptVersionType() {
-	console.log('Select the version type:')
-	console.log('1) M1 (Major)')
-	console.log('2) M2 (Minor)')
-	console.log('3) P0 (Patch)')
-
-	const input = await new Promise((resolve) => {
-		process.stdin.setEncoding('utf8')
-		process.stdin.once('data', (data) => resolve(data.trim()))
-	})
-
-	switch (input) {
-		case '1':
-			return 'M1'
-		case '2':
-			return 'M2'
-		case '3':
-			return 'P0'
-		default:
-			console.error('Invalid choice. Aborting push.')
-			process.exit(1)
-	}
+	return Number.parseInt(execSync(gitCommand).toString().trim(), 10) || 0
 }
 
 async function main() {
@@ -64,18 +67,41 @@ async function main() {
 		const currentMonth = (new Date().getMonth() + 1).toString().padStart(2, '0')
 		const totalCommits = getTotalCommitsForYear()
 
-		const versionType = await promptVersionType()
-		const newVersion = `${currentYear}.${currentMonth}.${totalCommits}.${versionType}`
+		console.log('Select the version type:')
+		console.log('[1] M1 (Major)')
+		console.log('[2] M2 (Minor)')
+		console.log('[3] P0 (Patch)')
 
+		const choice = await prompt('Enter your choice (1-3): ')
+
+		let versionType
+		switch (choice) {
+			case '1':
+				versionType = 'M1'
+				break
+			case '2':
+				versionType = 'M2'
+				break
+			case '3':
+				versionType = 'P0'
+				break
+			default:
+				console.error('Invalid choice. Aborting push.')
+				process.exit(1)
+		}
+
+		const newVersion = `${currentYear}.${currentMonth}.${totalCommits}.${versionType}`
 		console.log(`New version: ${newVersion}`)
 		await updateVersion(newVersion)
 
-		execSync(`git add ${packageJsonPath}`)
+		execSync(`git add ${pkgPath}`)
 		console.log('Version updated and staged for commit.')
 
+		process.stdin.destroy()
 		process.exit(0)
 	} catch (err) {
 		console.error('Error:', err.message)
+		process.stdin.destroy()
 		process.exit(1)
 	}
 }
