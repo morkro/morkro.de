e78205b1c408719c9feb4ed2fdd8ffeafcf49e2b
Moritz Kröger <write@morkro.de>
2025-03-01 15:31:26 +0100

    updates current job

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ export default {
 	email: 'write@morkro.de',
 	title: 'Hey there!',
 	description:
-		'I am Moritz Kröger, Director of Software & Cloud Engineering and Open Source contributor who lives in Berlin.',
+		'I am Moritz Kröger, Director of Engineering and Open Source contributor who lives in Berlin.',
 	url: 'https://morkro.de',
 	environment: process.env.NODE_ENV,
 	timestamp: Date.now(),
